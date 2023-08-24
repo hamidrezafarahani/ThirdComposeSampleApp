acf29fd94af5b4e05b653c08a6804f59b69ddd35
HamidReza Farahani <h.farahani>
2023-08-24 16:15:26 +0430

    handle configuration change

--- app/src/main/java/com/example/thirdcomposesampleapp/MainActivity.kt
+++ app/src/main/java/com/example/thirdcomposesampleapp/MainActivity.kt
@@ -6,23 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.thirdcomposesampleapp.ui.MySootheAppLandscape
+import com.example.thirdcomposesampleapp.ui.MySootheAppPortrait
 import com.example.thirdcomposesampleapp.ui.theme.ThirdComposeSampleAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val windowSizeClass = calculateWindowSizeClass(activity = this)
             ThirdComposeSampleAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MySootheApp(windowSize = windowSizeClass)
                 }
             }
         }
@@ -30,17 +35,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MySootheApp(
+    windowSize: WindowSizeClass
+) {
+    when (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            MySootheAppPortrait()
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ThirdComposeSampleAppTheme {
-        Greeting("Android")
+        WindowWidthSizeClass.Expanded -> {
+            MySootheAppLandscape()
+        }
     }
 }
+
